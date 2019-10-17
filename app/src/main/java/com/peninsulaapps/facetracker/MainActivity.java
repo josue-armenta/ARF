ac634c29479c2838f26bd6a1bf19e84b365cc031
Josue Armenta <josue@gmail.com>
2019-10-16 20:36:48 -0700

    Refactoring

--- app/src/main/java/com/peninsulaapps/facetracker/MainActivity.java
+++ app/src/main/java/com/peninsulaapps/facetracker/MainActivity.java
@@ -7,8 +7,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
